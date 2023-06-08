5c94a876e3c8fb3ce500d3df1423efca070240df
Trond Albinussen <trond.albinussen@blinq.no>
2023-06-08 14:49:14 +0200

    Add interface to pluck

--- src/components/pluck.ts
+++ src/components/pluck.ts
@@ -1,3 +1,7 @@
-export const pluck = (elements, field) => {
+interface props {
+    name: string;
+}
+
+export const pluck = (elements: props[], field: string) => {
     return elements.map((element) => element[field]);
 }
