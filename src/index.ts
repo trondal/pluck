361afc0230b6ed3075c9ed044d67965fb1494a91
Trond Albinussen <trond.albinussen@blinq.no>
2023-06-13 07:06:34 +0200

    Barrel export of Box and Badge

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { Badge } from './components/Badge/Badge';
+import { Box } from './components/Box/Box';
 import { add } from './components/add';
 import { subtract } from './components/subtract';
 
@@ -5,5 +7,5 @@ import { subtract } from './components/subtract';
 export { pluck } from './components/pluck';
 
 export {
-    add, subtract
+    add, subtract, Box, Badge
 }
